8d72f1227136bd1864e650ac110c3345a22f4acf
Nicole Kelly <nrkelly57@gmail.com>
2023-08-23 16:10:04 +1200

    all commit

--- JS Fundamentals/index.js
+++ JS Fundamentals/index.js
@@ -1,22 +1,24 @@
-/* console.log("" + 1 + 0); //10
-console.log("" - 1 + 0); //-1
-console.log(true + false); //1
-console.log(!true); //false
-console.log(6 / "3"); //2
-console.log("2" * "3"); //6
-console.log(4 + 5 + "px"); //9px
-console.log("$" + 4 + 5); //$45
-console.log("4" - 2); //2
-console.log("4px" - 2); //NaN
-console.log("-9" + 5); //-95
-console.log("-9" - 5); //-14
-console.log(null + 1); //1
-console.log(undefined + 1); //NaN
-console.log(undefined == 1); //NaN
-console.log(undefined === null); //false
-console.log("\t\n" - 2); //-2 
+// Question 1:
+// console.log("" + 1 + 0); //10
+// console.log("" - 1 + 0); //-1
+// console.log(true + false); //1
+// console.log(!true); //false
+// console.log(6 / "3"); //2
+// console.log("2" * "3"); //6
+// console.log(4 + 5 + "px"); //9px
+// console.log("$" + 4 + 5); //$45
+// console.log("4" - 2); //2
+// console.log("4px" - 2); //NaN
+// console.log("-9" + 5); //-95
+// console.log("-9" - 5); //-14
+// console.log(null + 1); //1
+// console.log(undefined + 1); //NaN
+// console.log(undefined == 1); //NaN
+// console.log(undefined === null); //false
+// console.log("\t\n" - 2); //-2 
 
 
+// Question 2:
 let three = "3";
 let four = "4";
 let thirty = "30";
@@ -27,19 +29,23 @@ let division = three / four;
 let subtraction = three - four;
 let lessThan1 = three < four;
 let lessThan2 = thirty < four;
-console.log(three + four); //34
-console.log(three * four); //12
-console.log(three / four); //0.75
-console.log(three - four); //-1
-console.log(three < four); //true
-console.log(thirty < four); //true 
-*/ 
-if (0) console.log ('#1 zero is true'); //will not print because 0 has a zero value, therefore it is false
-if ("0") console.log ('#2 zero is true'); //will print because "0" is a non-empty string, therefore it is true
-if (null) console.log ('null is true'); //will not print because null has no value, therefore it is false
-if (-1) console.log ('negative is true'); //will print because -1 is a non-zero number, therefore it is true
-if (1) console.log ('positive is true');//will print because 1 is a non-zero number, therefore it is true
-/*
+// console.log(three + four); //34
+// console.log(three * four); //12
+// console.log(three / four); //0.75
+// console.log(three - four); //-1
+// console.log(three < four); //true
+// console.log(thirty < four); //true 
+
+
+// Question 3:
+// if (0) console.log ('#1 zero is true'); //will not print because 0 has a zero value, therefore it is false
+// if ("0") console.log ('#2 zero is true'); //will print because "0" is a non-empty string, therefore it is true
+// if (null) console.log ('null is true'); //will not print because null has no value, therefore it is false
+// if (-1) console.log ('negative is true'); //will print because -1 is a non-zero number, therefore it is true
+// if (1) console.log ('positive is true');//will print because 1 is a non-zero number, therefore it is true
+
+
+// Question 4:
 let a = 2, b = 3; 
 let result = ` ${ a } + ${ b } is `; 
 
@@ -48,22 +54,24 @@ if (a + b < 10) {
 } else { 
     result += 'greater than 10' ; 
 }
-console.log(result);
+// console.log(result);
+// console.log(a + b < 10 ? result += 'less than 10' : result += 'greater than 10');
 
-console.log(a + b < 10 ? result += 'less than 10' : result += 'greater than 10');
 
-
+// Question 5:
 function getGreeting(name) { 
     return 'Hello ' + name + '!' ; 
 } 
 const getGreeting = function(name) {
     return 'Hello ' + name + '!' ;
 }
-console.log(getGreeting('Nicole')); 
+// console.log(getGreeting('Nicole')); 
 
 const getGreeting = (name) => 'Hello ' + name + '!';
-console.log(getGreeting('Nicole'));
+// console.log(getGreeting('Nicole'));
 
+
+// Question 6:
 const westley = { 
     name: 'Westley', 
     numFingers: 5 
@@ -78,7 +86,7 @@ const inigo = {
     lastName: 'Montoya',
     greeting(person) { 
         let greeting = `Hello ${person.name}, my name is ${this.firstName} ${this.lastName}.`; 
-        console.log(greeting + this.getCatchPhrase(person)); 
+        // console.log(greeting + this.getCatchPhrase(person)); 
     }, 
     
     getCatchPhrase(person) { 
@@ -91,6 +99,7 @@ const inigo = {
 inigo.greeting(westley) 
 inigo.greeting(rugen) 
 
+
 const westley = { 
     name: 'Westley', 
     numFingers: 5 
@@ -105,7 +114,7 @@ const inigo = {
     lastName: 'Montoya',
     greeting(person) { 
         let greeting = `Hello ${person.name}, my name is ${this.firstName} ${this.lastName}.`; 
-        console.log(greeting + this.getCatchPhrase(person)); 
+        // console.log(greeting + this.getCatchPhrase(person)); 
     }, 
     
     getCatchPhrase: (person) =>
@@ -115,6 +124,8 @@ const inigo = {
 inigo.greeting(westley);
 inigo.greeting(rugen); 
 
+
+// Quesetion 7:
 const basketballGame = { 
     score: 0,
     fouls: 0,
@@ -135,11 +146,11 @@ const basketballGame = {
         return this;
      },
      halfTime () { 
-        console.log('Halftime score is ' + this.score + ', fouls: ' + this.fouls); 
+        // console.log('Halftime score is ' + this.score + ', fouls: ' + this.fouls); 
         return this;
     },
     finalScore() {
-        console.log('Full-time final score is ' + this.score + ', fouls: ' + this.fouls);
+        // console.log('Full-time final score is ' + this.score + ', fouls: ' + this.fouls);
         return this;
     },
 };
@@ -148,6 +159,7 @@ const basketballGame = {
 basketballGame.basket().freeThrow().freeThrow().basket().threePointer().incrementFoul().halfTime().incrementFoul().freeThrow().freeThrow().finalScore();
 
 
+// Question 8:
 const sydney = { 
     name: 'Sydney', 
     population: 5_121_000, 
@@ -157,8 +169,8 @@ const sydney = {
 };
 
 for (let key in sydney) {
-    console.log('key: ' + key);
-    console.log('value: ' + sydney[key]);
+    // console.log('key: ' + key);
+    // console.log('value: ' + sydney[key]);
 };
 
 const denver = {
@@ -170,33 +182,34 @@ const denver = {
 };
 
 for (let key in denver) {
-    console.log('key: ' + key);
-    console.log('value: ' + denver[key]);
+    // console.log('key: ' + key);
+    // console.log('value: ' + denver[key]);
 };
 
+
+// Question 9:
 let teamSports = ['Hockey', 'Cricket', 'Volleyball']; 
 // let moreSports = teamSports;
 let moreSports = teamSports.slice();
 moreSports.push('Tennis');
 moreSports.unshift('Basketball');
-console.log('teamSports:', teamSports);
-console.log('moreSports:', moreSports);
-console.log('Original teamSports', teamSports);
-*/
-/*
+// console.log('teamSports:', teamSports);
+// console.log('moreSports:', moreSports);
+// console.log('Original teamSports', teamSports);
+
 let dog1 = 'Bingo'; 
 let dog2 = dog1;
 dog2 = 'Molly';
-console.log('dog1:', dog1);
-console.log('dog2:', dog2);
+// console.log('dog1:', dog1);
+// console.log('dog2:', dog2);
 
 let cat1 = {name: 'Fluffy', breed: 'Siberian'};
 let cat2 = cat1;
 let cat2 = Object.assign({}, cat1);
 cat2.name = 'Tom';
-console.log('cat1:', cat1);
-console.log('cat2:', cat2);
-console.log('Original cat1:', cat1);
+// console.log('cat1:', cat1);
+// console.log('cat2:', cat2);
+// console.log('Original cat1:', cat1);
 
 // teamSports: The teamSports array was modified using the moreSports (slice) variable, but the original array was unchanged because moreSports is a reference to the same array in memory. The changes made to moreSports didn't affect the original teamSports.
 // dog1: The dog1 variable was not modified in the code provided, so its value remains 'Bingo'.
@@ -204,6 +217,7 @@ console.log('Original cat1:', cat1);
 // In summary, changes made to variables referencing the same object in memory will affect that object regardless of which variable is used to make the change. However, variables that reference primitive values (like strings and numbers) store those values directly, so changes to one variable don't affect others.
 
 
+// Question 10:
 function Person (name, age) { 
     this.name = name; 
     this.age = age; 
@@ -217,9 +231,8 @@ function Person (name, age) {
 let Person1 = new Person('Kevin Hart', 44);
 let Person2 = new Person('Rowan Atkinson', 68);
 
-console.log('Person 1 can drive:', Person1.canDrive());
-console.log('Person 2 can drive:', Person2.canDrive());
-
+// console.log('Person 1 can drive:', Person1.canDrive());
+// console.log('Person 2 can drive:', Person2.canDrive());
 
 class PersonClass {
     constructor(name, age) {
@@ -236,7 +249,6 @@ let Person1 = new PersonClass('Kevin Hart', 44);
 let Person2 = new PersonClass('Rowan Atkinson', 68);
 let Person3 = new PersonClass('Meryl Streep', 77);
 
-console.log('Person 1 can drive:', Person1.canDrive());
-console.log('Person 2 can drive:', Person2.canDrive());
-console.log('Person 3 can drive:', Person3.canDrive());
-*/
+// console.log('Person 1 can drive:', Person1.canDrive());
+// console.log('Person 2 can drive:', Person2.canDrive());
+// console.log('Person 3 can drive:', Person3.canDrive());
